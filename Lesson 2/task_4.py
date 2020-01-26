054498ee87e455e0e66616a42f5c8470bdef818d
emelkhovsky <emelkhovsky4@mail.ru>
2020-01-26 13:23:07 +0300

    fix 4th task in lesson2

--- Lesson 2/task_4.py
+++ Lesson 2/task_4.py
@@ -1,11 +1,24 @@
-def range_realization(n):
+def range_realization(*args):
     range_list = []
+    step = 1
     i = 0
-    while i < n:
+    limit = args[0]
+    len_args = len(args)
+    if len_args == 2:
+        i = args[0]
+        limit = args[1]
+    elif len_args == 3:
+        i = args[0]
+        step = args[2]
+        limit = args[1]
+    while i < limit:
         range_list.append(i)
-        i += 1
+        i += step
     return range_list
 
-n = 4
-rezult = range_realization(n)
-print(rezult) #[0, 1, 2, 3]
+rezult = range_realization(4,10)
+print(rezult) #[4, 5, 6, 7, 8, 9]
+rezult = range_realization(10)
+print(rezult) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+rezult = range_realization(4,10,2)
+print(rezult) #[4, 6, 8]
