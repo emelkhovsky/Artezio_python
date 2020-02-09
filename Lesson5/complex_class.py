24f29591864422b3478948a01f4f4886458a1920
emelkhovsky <emelkhovsky4@mail.ru>
2020-02-09 14:45:31 +0300

    add complex and student

---
+++ Lesson5/complex_class.py
@@ -0,0 +1,44 @@
+class Complex(object):
+
+    def __init__(self, x=1, y=1):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        z = '+'
+        if self.y < 0:
+            z = ''
+        return format(self.x, '.2f') + z + format(self.y, '.2f') + 'i'
+
+    def __add__(self, other):
+        return Complex(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Complex(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Complex(self.x * other.x - self.y * other.y, other.x * self.y + self.x * other.y)
+
+    def __truediv__(self, other):
+        r_n = other.x ** 2 + other.y ** 2
+        r1 = self.x * other.x + self.y * other.y
+        r2 = self.y * other.x - self.x * other.y
+        return Complex(r1/r_n, r2/r_n)
+
+    def __abs__(self):
+        return Complex((self.x ** 2 + self.y ** 2) ** 0.5, 0)
+
+
+def main(s1, s2):
+    c = Complex(int(s1.split()[0]), int(s1.split()[1]))
+    d = Complex(int(s2.split()[0]), int(s2.split()[1]))
+    print(c + d)
+    print(c - d)
+    print(c * d)
+    print(c / d)
+    print(abs(c))
+    print(abs(d))
+
+ss1 = '2 1'
+ss2 = '5 6'
+main(ss1, ss2)
---
+++ Lesson5/student_class.py
@@ -0,0 +1,52 @@
+class Student(object):
+
+    def __init__(self, name, info_dict):
+        self.name = name
+        self.info_dict = info_dict
+        self.sum = 0
+        self.points = [0] * self.info_dict.get('lab_num') #список баллов за каждую лабу
+        self.attempts = [0] * self.info_dict.get('lab_num') #список попыток за каждую лабу соответственно
+
+    def make_lab(self, m, n=0):
+        if 0 <= n < self.info_dict.get('lab_num') - 1:
+            if 0 < m <= self.info_dict.get('lab_max'):
+                if self.attempts[n] < 3: #ограничение в 3 попытки
+                    self.points[n] = m
+                    self.attempts[n] += 1
+        return self
+
+    def make_exam(self, m):
+        if 0 < m <= self.info_dict.get('exam_max'):
+            self.sum += m
+        return self
+
+    def is_sertified(self):
+        for i in self.points:
+            self.sum += i
+        rez = True
+        if self.sum / 100 < self.info_dict.get('k'):
+            rez = False
+        return self.name, self.sum, rez
+
+
+def main():
+    student_name = 'Vlad'
+    course_info = {'exam_max': 30, 'lab_max': 7, 'lab_num': 10, 'k': 0.61}
+    vlad = Student(student_name, course_info)
+    vlad.make_lab(6, 2)
+    vlad.make_lab(5, 3)
+    vlad.make_lab(7, 3)
+    vlad.make_lab(7, 4)
+    vlad.make_lab(6)
+    vlad.make_lab(6)
+    vlad.make_lab(6)
+    vlad.make_lab(6) #Четвертая попытка уже не учитывается
+    vlad.make_lab(6, 8)
+    vlad.make_lab(7, 5)
+    vlad.make_exam(30)
+    rezult = vlad.is_sertified()
+    return rezult
+
+
+rezult = main()
+print(rezult)
